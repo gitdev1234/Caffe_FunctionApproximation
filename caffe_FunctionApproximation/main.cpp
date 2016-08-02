684e942a42b8f908cfdd61313bca47956d784158
root <root@metaldesk>
2016-08-02 10:32:12 +0200

    test runs now with nearly equal

--- caffe_FunctionApproximation/main.cpp
+++ caffe_FunctionApproximation/main.cpp
@@ -8,10 +8,42 @@ using namespace std;
 // !!!! TEST_CASE IN COMBINITION WITH REQUIRE IS LIKE A LOOP STRUCTURE !!!!
 // !!!! THE TEST_CASE IS EXECUTED FOR EVERY SECTION !!!!
 
+bool nearlyEqual(double val1_, double val2_, double tolerance_) {
+    return ( abs (val1_ - val2_) <= tolerance_ );
+}
+
+TEST_CASE( "Test nearly equal" ) {
+    const double TOLERANCE = 0.05;
+    REQUIRE(nearlyEqual(0, 0.05,TOLERANCE));
+    REQUIRE(nearlyEqual(0,-0.05,TOLERANCE));
+    REQUIRE(nearlyEqual(0, 0.04,TOLERANCE));
+    REQUIRE(nearlyEqual(0,-0.04,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual(0, 0.06,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual(0,-0.06,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.02,-0.07,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.02, 0.03,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.02,-0.06,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.02, 0.02,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual(-0.02,-0.08,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual(-0.02, 0.04,TOLERANCE));
+
+    REQUIRE(nearlyEqual( 0.05,0,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.05,0,TOLERANCE));
+    REQUIRE(nearlyEqual( 0.04,0,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.04,0,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual( 0.06,0,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual(-0.06,0,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.07,-0.02,TOLERANCE));
+    REQUIRE(nearlyEqual( 0.03,-0.02,TOLERANCE));
+    REQUIRE(nearlyEqual(-0.06,-0.02,TOLERANCE));
+    REQUIRE(nearlyEqual( 0.02,-0.02,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual(-0.08,-0.02,TOLERANCE));
+    REQUIRE_FALSE(nearlyEqual( 0.04,-0.02,TOLERANCE));
+}
 
 
 TEST_CASE( "Simple Forward Net scalar input Value -> tanh -> scalar output value" ) {
-    ANN ann("../caffe_FunctionApproximation/prototxt/input_tanh_output.prototxt");
+    ANN ann("../caffe_FunctionApproximation/prototxt/very_simple_net.prototxt");
 
 
     SECTION( "single input works" ) {
@@ -95,7 +127,7 @@ TEST_CASE("Training ANN") {
            vector<double> inputVals;
            vector<double> tanhOut;
            vector<double> annOut;
-           ann.setNetStructurePrototxtPath("../caffe_FunctionApproximation/prototxt/net_without_loss.prototxt");
+
            while (d <= 2.0) {
                d += 0.1;
                inputVals.push_back(d);
@@ -109,6 +141,7 @@ TEST_CASE("Training ANN") {
                oFile << inputVals[i] << "," << tanhOut[i] << "," << annOut[i] << endl;
                cout << "for : " << inputVals[i] << " tanh : "  << tanhOut[i] << endl;
                cout << "for : " << inputVals[i] << " ann  : "  << annOut[i] << endl;
+               REQUIRE(nearlyEqual(tanhOut[i],annOut[i],0.05));
            }
            oFile.close();
        }
@@ -116,4 +149,3 @@ TEST_CASE("Training ANN") {
 
 
 }
-
